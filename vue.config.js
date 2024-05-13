49789f0e1c0c03f861a262d0b728af64951da6da
Pablo Jelvez <pjelvezj@gmail.com>
2024-05-13 12:27:41 -0400

    Se coloca el slash a la ruta en vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-    publicPath: "desafio-guiado-binding-formularios",
+    publicPath: "/desafio-guiado-binding-formularios",
     outputDir: "docs"
   
 })
