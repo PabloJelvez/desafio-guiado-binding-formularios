ea83d457d3a8d4270732c98a0bb16b0081b9da5f
Pablo Jelvez <pjelvezj@gmail.com>
2024-05-13 12:09:43 -0400

    Prueba docs

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/desafio-guiado-binding-formularios",
-  outputDir: "docs"
+    publicPath: "desafio-guiado-binding-formularios",
+    outputDir: "docs"
+  
 })
